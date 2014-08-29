5b7835e40eb384bae1c6c4f627ebace1b3e21f3a
Victor Alenkov <Alenkov.Victor@gmail.com>
2014-08-29 12:18:32 +0400

    add another method from only ChildClass

--- src/main/java/ru/itbasis/demo/java/self/child/ChildClass.java
+++ src/main/java/ru/itbasis/demo/java/self/child/ChildClass.java
@@ -1,7 +1,8 @@
 package ru.itbasis.demo.java.self.child;
 
-public class ChildClass extends BaseClass<ChildClass>  {
+public class ChildClass extends BaseClass<ChildClass> {
     private int childParam;
+    private int anotherChildParam;
 
     public int getChildParam() {
         return childParam;
@@ -9,6 +10,15 @@ public class ChildClass extends BaseClass<ChildClass>  {
 
     public ChildClass setChildParam(int i) {
         this.childParam = i;
+        return this;
+    }
+
+    public int getAnotherChildParam() {
+        return anotherChildParam;
+    }
+
+    public ChildClass setAnotherChildParam(int i) {
+        this.anotherChildParam = i;
         return this;
     }
 }
--- src/test/java/ru/itbasis/demo/java/self/child/ClassTest.java
+++ src/test/java/ru/itbasis/demo/java/self/child/ClassTest.java
@@ -6,25 +6,35 @@ import org.testng.annotations.Test;
 
 public class ClassTest {
 
-    public static final int BASE_PARAM  = 1;
-    public static final int CHILD_PARAM = 2;
+    public static final int BASE_PARAM          = 1;
+    public static final int CHILD_PARAM         = 2;
+    public static final int ANOTHER_CHILD_PARAM = 3;
 
     @DataProvider(name = "data")
     public static Object[][] data() {
 
         final ChildClass item0 = new ChildClass().setBaseParam(BASE_PARAM);
-        item0.setChildParam(CHILD_PARAM);
+        item0.setChildParam(CHILD_PARAM).setAnotherChildParam(ANOTHER_CHILD_PARAM);
 
-        final ChildClass item1 = new ChildClass().setChildParam(CHILD_PARAM).setBaseParam(BASE_PARAM);
+        final ChildClass item1 =
+            new ChildClass().setChildParam(CHILD_PARAM).setBaseParam(BASE_PARAM).setAnotherChildParam(
+                ANOTHER_CHILD_PARAM);
 
-        final ChildClass item2 = new ChildClass().setBaseParam(BASE_PARAM).setChildParam(CHILD_PARAM);
+        final ChildClass item2 =
+            new ChildClass().setBaseParam(BASE_PARAM).setChildParam(CHILD_PARAM).setAnotherChildParam(
+                ANOTHER_CHILD_PARAM);
 
-        return new Object[][]{{item0}, {item1}, {item2}};
+        final ChildClass item3 =
+            new ChildClass().setAnotherChildParam(ANOTHER_CHILD_PARAM).setBaseParam(BASE_PARAM).setChildParam(
+                CHILD_PARAM);
+
+        return new Object[][]{{item0}, {item1}, {item2}, {item3}};
     }
 
     @Test(dataProvider = "data")
-    public void testChildClass(final IChildParam item) throws Exception {
+    public void testChildClass(final ChildClass item) throws Exception {
         Assert.assertEquals(item.getBaseParam(), BASE_PARAM);
         Assert.assertEquals(item.getChildParam(), CHILD_PARAM);
+        Assert.assertEquals(item.getAnotherChildParam(), ANOTHER_CHILD_PARAM);
     }
 }
